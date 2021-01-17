a1ef6fcf0c3fd25e3b0d1b0da3311b24180b6af7
Vadim Ershov <vadim.ershov.01@mail.ru>
2021-01-17 22:21:25 +0500

    start Spring bot VK

---
+++ src/main/java/com/vadim01er/springbotvk/SpringBotVkApplication.java
@@ -0,0 +1,13 @@
+package com.vadim01er.springbotvk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBotVkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBotVkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vadim01er/springbotvk/SpringBotVkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vadim01er.springbotvk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBotVkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
