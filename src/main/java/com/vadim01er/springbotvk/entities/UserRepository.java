27ed24f07cfda3e6a96c746f83d03e76179a0533
Vadim Ershov <vadim.ershov.01@mail.ru>
2021-01-26 03:06:22 +0500

    second 1

--- src/main/java/com/vadim01er/springbotvk/entities/UserRepository.java
+++ src/main/java/com/vadim01er/springbotvk/entities/UserRepository.java
@@ -1,6 +1,8 @@
 package com.vadim01er.springbotvk.entities;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
