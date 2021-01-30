142b9a1c86898699afd0b2817652526d3d94e07e
Vadim Ershov <vadim.ershov.01@mail.ru>
2021-01-31 03:06:33 +0500

    add ping 2

--- src/main/java/com/vadim01er/springbotvk/controller/Controller.java
+++ src/main/java/com/vadim01er/springbotvk/controller/Controller.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
